f87a98952f915b7763545a33814b990232a44b71
lexctk <alexandra.derevko@gmail.com>
2018-12-27 22:43:37 +0100

    Project Setup
    * Add components
    * Add routing
    * Add mock authentication service

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { AuthGuardService } from './auth-guard.service';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'form', canActivate: [AuthGuardService], component: FormBuilderComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FormBuilderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
